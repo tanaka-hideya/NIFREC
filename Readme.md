d25c08c4ad03c9d4f7843fbdd778e600c2acf3c3
Tomoyuki Miyao <miyaotomoyuki1985@gmail.com>
2025-06-08 22:32:15 +0900

    done. Make environment and module 1_1

--- Readme.md
+++ Readme.md
@@ -20,12 +20,53 @@ cd NIFREC
 conda env create -f environment.yml
 ```
 
-1. Activate the `nifrec` environment by
+2. Activate the `nifrec` environment and add the script folder to the `PYTHOPATH`
 ```bash
 conda activate nifrec
+NIFRECDIR=$(pwd)
+export PYTHONPATH="$PYTHONPATH:$NIFRECDIR/src/nifrec"
 ```
 
-# Running geometry optimization
+3. Test whether modules can be loaded.
+```bash
+python -m 1_1_nifrec_rdkit --help
+```
+Then, following output shows
+
+```
+usage: nifrec_rdkit [-h] [--infile INFILE] [--smicol SMICOL] [--idxcol IDXCOL] [--outfolder OUTFOLDER] [--outfile OUTFILE]
+                    [--nconfs NCONFS] [--rmsd-thres RMSD_THRES] [--njobs NJOBS] [--backend BACKEND]
+
+Conformer generator using RDKit
+
+options:
+  -h, --help            show this help message and exit
+  --infile INFILE       Input csv filename
+  --smicol SMICOL       Smiles column for structure generation
+  --idxcol IDXCOL       Index column idx (0 starts)
+  --outfolder OUTFOLDER
+                        Output folder where conformers are stored
+  --outfile OUTFILE     Output csv filename without extension (i.e. csv)
+  --nconfs NCONFS       Number of maximum conformers to be generated
+  --rmsd-thres RMSD_THRES
+                        RMSD threshold between generated conformers
+  --njobs NJOBS         Number of parallel computation threads (-1 means all cores)
+  --backend BACKEND     Parallel computation backend (loky or multiprocessing)
+```
+
+# Running geometry optimization using an example file.
+From the SMILES strings in `data/sample.csv`, conformer generation and optimization will be conducted.
+
+```bash
+mkdir results
+```
+
+## Step 1: RDkit conformre generation
+```
+python -m 1_1_nifrec_rdkit --infile data/sample.csv --smicol smi --outfolder results/rdkit-confgen --backend multiprocessing
+```
+Then, you can find the `rdkit-confgen` folder under the `results` directory, where the conformation generation results are stored.
+
 
 
 
--- src/nifrec/1_1_nifrec_rdkit.py
+++ src/nifrec/1_1_nifrec_rdkit.py
@@ -1,11 +1,11 @@
 """
 File: 1_1_nifrec_rdkit.py
-Author: Hideya Tanaka
+Author: Hideya Tanaka at Nara Institute of Science and Technology
 Supervised by: Tomoyuki Miyao
 Description:
     This script performs the conformer generation using RDKit.
 """
-
+import argparse
 import os
 import numpy as np
 import pandas as pd
@@ -14,17 +14,15 @@ from morfeus.conformer import ConformerEnsemble
 from utility_opt import MakeFolder, SmilesToCanSmiles, WriteMolsSDF
 import sys
 
-def process_rows_for_rdkit(fd, file_path_input, file_path_output, n_confs, thres, njobs=-1, standardize=True, backend='loky'):
-    outfd = MakeFolder(f'{fd}/rdkit', allow_override=True) 
-    smicol = 'smiles'
-    original_df = pd.read_csv(file_path_input, index_col=0)
+def process_rows_for_rdkit(fd, file_path_input, file_path_output, n_confs, thres, njobs=-1,  backend='loky',smicol='smiles', idxcol=0):
+    outfd       = MakeFolder(f'{fd}/rdkit', allow_override=True) 
+    original_df = pd.read_csv(file_path_input, index_col=idxcol)
     print(f'Loaded mols: {len(original_df)}')
     df = original_df.copy()
     
     # Remove duplicates
-    if standardize:
-        df[smicol] = original_df[smicol].apply(SmilesToCanSmiles)
-        df.drop_duplicates(subset=smicol, inplace=True)
+    df[smicol] = original_df[smicol].apply(SmilesToCanSmiles)
+    df.drop_duplicates(subset=smicol, inplace=True)
     print(f'Loaded mols after removing duplicates: {len(df)}')
     
     # Parallel calculation setting     
@@ -42,7 +40,7 @@ def process_rows_for_rdkit(fd, file_path_input, file_path_output, n_confs, thres
     if combined_df[smicol].equals(combined_df['smiles_input_rdkit_confgen']):
         combined_df = combined_df.drop(columns='smiles_input_rdkit_confgen')
         print('success: combine')
-    combined_df.to_csv(file_path_output)
+    combined_df.to_csv(f'{fd}/{file_path_output}')
 
 def generate_conf_rdkit(workerid, pds_smiles, nconfs, rseed, rmsdthres, outfd_xyz, outfd_sdf, outfd):
     ntotal = len(pds_smiles)
@@ -68,22 +66,62 @@ def generate_conf_rdkit(workerid, pds_smiles, nconfs, rseed, rmsdthres, outfd_xy
             status_dict[number] = {'smiles_input_rdkit_confgen': smiles, 'success_rdkit_confgen': False, 'nconfs_rdkit_confgen': None}
     return pd.DataFrame.from_dict(status_dict, orient='index')
 
+
 if __name__ == '__main__':
-    fd = os.path.dirname(os.path.abspath(__file__))
-    
-    # ========== Settings (CHANGE HERE) ==========
-    # The maximum number of generated conformers
-    n_confs = 20
-    
-    # RMSD threshold for pruning
-    thres = 1
-    
-    # Enter the file paths
-    file_path_input     = f'{fd}/input_smiles.csv'
-    file_path_output    = f'{fd}/rdkit_stats.csv'
-    # =============================================
-    
+
+    parser = argparse.ArgumentParser(
+                        prog='nifrec_rdkit',
+                        description= 'Conformer generator using RDKit'
+    )
+    parser.add_argument('--infile',
+                     help='Input csv filename',
+                     type=str
+                     )
+    parser.add_argument('--smicol',
+                     help='Smiles column for structure generation',
+                     type=str,
+                     default='smiles'
+                     )
+    parser.add_argument('--idxcol',
+                     help='Index column idx (0 starts)',
+                     type=int,
+                     default=0
+                     )
+    parser.add_argument('--outfolder',
+                     help='Output folder where conformers are stored',
+                     type=str,
+                     default='rdkit-confgen'
+                     )
+    parser.add_argument('--outfile',
+                     help='Output csv filename without extension (i.e. csv)',
+                     type=str,
+                     default='rdkit_stats'
+                     )
+    parser.add_argument('--nconfs',
+                     help='Number of maximum conformers to be generated',
+                     type=int,
+                     default=20,
+                     )
+    parser.add_argument('--rmsd-thres',
+                     help='RMSD threshold between generated conformers',
+                     type=float,
+                     default=1,
+                     )
+    parser.add_argument('--njobs',
+                     help='Number of parallel computation threads (-1 means all cores)',
+                     type=int,
+                     default=-1,
+                     )
+    parser.add_argument('--backend',
+                     help='Parallel computation backend (loky or multithprocessing)',
+                     type=str,
+                     default='loky',
+                     )
+    p = parser.parse_args(sys.argv[1:])
+
+    fd = p.outfolder
+    fd = MakeFolder(fd, allow_override=True)
     sys.stdout = open(f'{fd}/log_rdkit.txt', 'w')
-    process_rows_for_rdkit(fd, file_path_input, file_path_output, n_confs, thres, njobs=-1, backend='loky')
+    process_rows_for_rdkit(fd, p.infile, p.outfile, p.nconfs, p.rmsd_thres, njobs=p.njobs, backend=p.backend,smicol=p.smicol)
     print('Finish')
     sys.stdout.close()
